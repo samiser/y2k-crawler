0fae15c4f7219f45b5b9dd2f9bc17f33fec0acb8
Samiser <github@me.samiser.xyz>
2026-03-29 14:27:21 +0000

    added terminal to freeze firewalls and made a simple puzzle using the mechanic

--- firewall.gd
+++ firewall.gd
@@ -1,9 +1,12 @@
 extends Node3D
 class_name FirewallEnemy
 
+@onready var sprite: Sprite3D = $Sprite3D
+
 @export var grid_path: NodePath
 @export var player_path: NodePath
-@export var move_duration := 0.25
+@export var terminal_paths: Array[NodePath]
+@export var move_duration := 0.19
 
 enum PatrolAxis { X, Z }
 @export var patrol_axis := PatrolAxis.X
@@ -15,6 +18,10 @@ var grid: Grid
 var player: Player
 var grid_pos := Vector2i.ZERO
 var _is_busy := false
+var _disabled := false:
+	set(value):
+		_disabled = value
+		sprite.frame = 1 if value else 0
 
 func _ready() -> void:
 	add_to_group("firewall_enemies")
@@ -23,6 +30,9 @@ func _ready() -> void:
 		grid = get_node(grid_path) as Grid
 	if player_path:
 		player = get_node(player_path) as Player
+	for path in terminal_paths:
+		var terminal := get_node(path) as Terminal
+		terminal.interacted.connect(_on_terminal_interacted)
 
 	if grid:
 		grid_pos = grid.world_to_grid(position)
@@ -32,8 +42,12 @@ func _ready() -> void:
 	if player:
 		player.moved.connect(_on_player_moved)
 
+func _on_terminal_interacted() -> void:
+	_disabled = not _disabled
+	_on_player_moved(player.grid_pos)
+
 func _on_player_moved(_player_new_pos: Vector2i) -> void:
-	if _is_busy or not player or not grid:
+	if _disabled or _is_busy or not player or not grid:
 		return
 
 	var target_grid_pos := grid_pos
---
+++ terminal.gd
@@ -0,0 +1,28 @@
+extends Node3D
+class_name Terminal
+
+signal interacted
+
+@export var grid_path: NodePath
+@export var player_path: NodePath
+
+var grid: Grid
+var player: Player
+var grid_pos := Vector2i.ZERO
+
+func _ready() -> void:
+	if grid_path:
+		grid = get_node(grid_path) as Grid
+	if player_path:
+		player = get_node(player_path) as Player
+
+	if grid:
+		grid_pos = grid.world_to_grid(position)
+
+func _on_area_input_event(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if not player:
+		return
+
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if player.grid_pos == grid_pos:
+			interacted.emit()
