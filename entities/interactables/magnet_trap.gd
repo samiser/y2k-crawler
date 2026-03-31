ab03210752b02699da3304c0e4dffc9be3d1ee41
wookbee <159189510+wookbee@users.noreply.github.com>
2026-03-31 14:54:22 +0100

    added some magnet logic, pulls in nearby coins

--- entities/interactables/magnet_trap.gd
+++ entities/interactables/magnet_trap.gd
@@ -6,7 +6,9 @@ var grid: Grid
 var grid_pos := Vector2i.ZERO
 var player: Player
 var life : int = 4
+
 @onready var sprite_3d: Sprite3D = $Sprite3D
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 func _ready() -> void:
 	add_to_group("magnets")
@@ -23,9 +25,34 @@ func _ready() -> void:
 
 func _on_player_moved(_player_new_pos: Vector2i) -> void:
 	life -= 1
-	print("shit moved bruh")
+
 	if life == 0:
 		_die()
+	else:
+		_zap()
+
+func _zap() -> void:
+	audio_stream_player_3d.play()
+	
+	for coin in get_tree().get_nodes_in_group("coins"):
+		if (coin.global_position.distance_to(global_position) < 6.0):
+			if grid.has_line_of_sight(grid_pos, coin.grid_pos):
+				grid.unregister_item(coin.grid_pos, coin)
+				
+				coin.grid_pos = grid_pos
+				
+				var ran_offset_magnitude : float = 0.6
+				var pos_ran_offset : Vector3 = Vector3(randf_range(-ran_offset_magnitude, ran_offset_magnitude), 0, randf_range(-ran_offset_magnitude, ran_offset_magnitude))
+				grid.register_item(grid_pos, coin)
+				
+				var tween := get_tree().create_tween()
+				tween.tween_property(coin, "position", grid.grid_to_world(grid_pos) + pos_ran_offset, 0.4)
+	
+	
+	
+	var tween := get_tree().create_tween()
+	tween.tween_property(sprite_3d, "position:y", 1.2, 0.5)
+	tween.tween_property(sprite_3d, "position:y", 0.5, 0.5)
 
 func _die():
 	queue_free()
