3d628da90f082a8b330b8193761d42f1a0297bb9
wookbee <159189510+wookbee@users.noreply.github.com>
2026-04-01 16:56:38 +0100

    WAH

--- entities/interactables/fireball.gd
+++ entities/interactables/fireball.gd
@@ -4,9 +4,10 @@ class_name Fireball
 var wall : FirewallEnemy
 
 func _process(delta: float) -> void:
-	var dir := (global_position - wall.global_position).normalized()
-	while global_position.distance_to(wall.global_position) > 1.0:
+	var dir := (wall.global_position - global_position).normalized()
+	if global_position.distance_to(wall.global_position) > 1.0:
 		global_position += dir * delta * 20.0
+	else:
 		_impact()
 	
 func _impact() -> void:
