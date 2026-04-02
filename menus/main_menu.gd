a1a5a02f4987cad6b5265afa94a6cf0717e78171
wookbee <159189510+wookbee@users.noreply.github.com>
2026-04-02 17:41:21 +0100

    play button now loads intro scene instead of main

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var play_button: Button = $CenterContainer/VBoxContainer/PlayButton
 const MAIN = preload("uid://up8x05j0vexy")
+const INTRO = preload("uid://up7g4gu006mi")
 
 func _ready() -> void:
-	play_button.pressed.connect(func() -> void: get_tree().change_scene_to_packed(MAIN))
+	play_button.pressed.connect(func() -> void: get_tree().change_scene_to_packed(INTRO))
