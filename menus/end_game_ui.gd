bfa071eb06eb3ca75bbde00a25f96541f7acf0ab
Samiser <github@me.samiser.xyz>
2026-04-02 20:51:33 +0000

    added controller support to end game ui

--- menus/end_game_ui.gd
+++ menus/end_game_ui.gd
@@ -10,11 +10,16 @@ func _ready() -> void:
 	add_to_group("ui_window")
 	hide()
 	close_requested.connect(_on_close)
+	visibility_changed.connect(_on_visibility_changed)
 	cancel_button.pressed.connect(_on_close)
-	ok_button.pressed.connect(func() -> void: 
-		_on_close() 
+	ok_button.pressed.connect(func() -> void:
+		_on_close()
 		end_game.emit()
 	)
+
+func _on_visibility_changed() -> void:
+	if visible:
+		ok_button.grab_focus()
 
 func _on_close() -> void:
 	closed.emit()
